48082577d4850166846b4f9709e837be98baada5
Chris Guy <christien.guy@gmail.com>
2019-06-25 15:30:04 +0100

    Fix responses that are consuming api to use new structure

--- src/pages/Result.js
+++ src/pages/Result.js
@@ -1,52 +1,56 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BASE_URL, COLOURS } from "constants.js";
+import { BASE_URL } from "constants.js";
 import NavBar from "components/NavBar";
-import { Referent, ResultsWrapper, SongWrapper, SongTitle, SongLine } from './Results.styles';
+import {
+    Referent,
+    ResultsWrapper,
+    SongWrapper,
+    SongTitle,
+    SongLine
+} from "./Results.styles";
 
 const Result = ({ resultId }) => {
     const [songData, setSongData] = useState(null);
-    
+
     useEffect(() => {
-        axios.get(`${BASE_URL}getWarpedSong?songId=${resultId}`).then(result => {
-            setSongData(result.data);
-        });
+        axios
+            .get(`${BASE_URL}getWarpedSong?songId=${resultId}`)
+            .then(result => {
+                setSongData(result.data);
+            });
     }, [resultId]);
 
     return (
         <ResultsWrapper>
             <NavBar />
-            <div>
-                <SongTitle>Song Title</SongTitle>
-            </div>
             {songData ? (
-                <div>
-                    <div><SongLine>{songData.artistName}</SongLine></div>
-                </div>
-            ) : null}
-            {songData ? (
-                <SongWrapper>
-                    {songData.warped.map(({ text, referentId }) => {
-                        if (referentId) {
-                            return (
-                                <>
-                                    <Referent
-                                        href={`/referent/${referentId}`}
-                                        data-referent-id={referentId}
-                                        dangerouslySetInnerHTML={{
-                                            __html: text.replace(
-                                                /(?:\r\n|\r|\n)/g,
-                                                "<br />"
-                                            )
-                                        }}
-                                    />
-                                    <br />
-                                </>
-                            );
-                        }
-                        return <p>{text}</p>;
-                    })}
-                </SongWrapper>
+                <>
+                    <SongTitle>{songData.title}</SongTitle>
+                    <SongLine>{songData.artistName}</SongLine>
+                    <SongWrapper>
+                        {songData.lyrics.warped.map(({ text, referentId }) => {
+                            if (referentId) {
+                                return (
+                                    <>
+                                        <Referent
+                                            href={`/referent/${referentId}`}
+                                            data-referent-id={referentId}
+                                            dangerouslySetInnerHTML={{
+                                                __html: text.replace(
+                                                    /(?:\r\n|\r|\n)/g,
+                                                    "<br />"
+                                                )
+                                            }}
+                                        />
+                                        <br />
+                                    </>
+                                );
+                            }
+                            return <p>{text}</p>;
+                        })}
+                    </SongWrapper>
+                </>
             ) : null}
         </ResultsWrapper>
     );
