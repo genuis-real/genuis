49f8b41b152962e5ee41fdafb7b1df56f3669e24
Chris Guy <christien.guy@gmail.com>
2019-07-09 15:15:25 +0100

    Add favicons and manifest

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { createGlobalStyle } from "styled-components";
-import { COLOURS } from 'constants.js';
+import { COLOURS } from "constants.js";
 
 const GlobalStyle = createGlobalStyle`
     body {
@@ -39,4 +39,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
