6487a6e26db48ba292ff76c41e7f6233e15576c5
Jack Gill <jackgill@trustedhousesitters.com>
2020-05-07 10:12:16 +0100

    Tidies up Start and converts it to TypeScript.

--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,8 +1,6 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import axios from "axios";
+import React from "react";
+
 // components
-import ResultsItem from "components/ResultsItem";
 import NavBar from "components/NavBar";
 import {
     Wrapper,
@@ -11,32 +9,25 @@ import {
     ReadyButton,
     ReadyLinkWrapper,
 } from "./Start.styles";
+import { RouteComponentProps } from "@reach/router";
 
-import { BASE_URL } from "constants.js";
+type Props = RouteComponentProps;
 
-class Start extends Component {
-    clickedReady = () => {
-        console.log('Clicked');
-    };
-
-    render() {
-        return (
-            <Wrapper>
+const Start: React.FC<Props> = () => {
+    return (
+        <Wrapper>
                 <NavBar />
                 <HypeWrapper>
                     <HypeSubText>Put yourself to the test by guessing the names of your favourite artist's songs, with lyrics like you've never seen them before.</HypeSubText>
                     <HypeSubText>Are you ready?</HypeSubText>
                     <ReadyLinkWrapper to="/">
-                        <ReadyButton
-                            onClick={this.clickedReady}
-                        >
+                        <ReadyButton>
                             I'M READY!
                         </ReadyButton>
                     </ReadyLinkWrapper>
                 </HypeWrapper>
             </Wrapper>
-        );
-    }
-}
+    );
+};
 
 export default Start;
