1db0beac89a99e2ac2e6ad23d29dc7324a88bca7
Jack Gill <jackgill@trustedhousesitters.com>
2020-05-20 15:42:28 +0100

    Fixes share icon styles.

--- src/components/Results.styles.ts
+++ src/components/Results.styles.ts
@@ -4,13 +4,13 @@ import { ReactComponent as TwitterIconOriginal } from "../img/twitter.svg";
 
 const TwitterIcon = styled(TwitterIconOriginal)`
     fill: ${({ theme }) => theme.COLOURS.accent};
-    height: 25vh;
+    width: 100%;
     margin: 24px;
 `;
 
 const FacebookIcon = styled(FacebookIconOriginal)`
     fill: ${({ theme }) => theme.COLOURS.accent};
-    height: 25vh;
+    width: 100%;
     margin: 24px;
 `;
 
