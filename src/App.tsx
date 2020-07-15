be5f4a4eca3af75a6d7babbead3313cd81622e8a
Chris Guy <christien.guy@gmail.com>
2020-07-15 17:04:32 +0100

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,38 @@
 import React, { Suspense } from "react";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import "@reach/combobox/styles.css";
+import styled from "styled-components";
 
 const Home = React.lazy(() => import("pages/Home"));
 const SearchResult = React.lazy(() => import("pages/SearchResult"));
 const Search = React.lazy(() => import("pages/Search"));
 
+const Nav = styled.nav`
+    display: flex;
+    padding: 0;
+
+    a {
+        color: white;
+        padding: 16px;
+    }
+`;
+
 const App: React.FC = () => {
     return (
-        <Suspense fallback={<p data-testid="loading">Loading....</p>}>
-            <Router>
-                <Home path="/" />
-                <Search path="/search">
-                    <SearchResult path=":songId" />
-                </Search>
-            </Router>
-        </Suspense>
+        <>
+            <Nav>
+                <Link to="/">Game</Link>
+                <Link to="/search">Search</Link>
+            </Nav>
+            <Suspense fallback={<p data-testid="loading">Loading....</p>}>
+                <Router>
+                    <Home path="/" />
+                    <Search path="/search">
+                        <SearchResult path=":songId" />
+                    </Search>
+                </Router>
+            </Suspense>
+        </>
     );
 };
 
